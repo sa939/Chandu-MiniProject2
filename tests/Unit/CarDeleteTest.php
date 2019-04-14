658da5cb27b6b63567b14e9f1ae990de4d6f504e
Chandu <sa939@njit.edu>
2019-04-14 19:43:30 -0400

    11. Create a unit test to test deleting a car.

---
+++ tests/Unit/CarDeleteTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+
+class CarDeleteTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+    /** @test */
+    public function cardeletetest()
+    {
+        $car = Car::find(2);
+        $this->assertTrue($car->delete());
+
+    }
+}
