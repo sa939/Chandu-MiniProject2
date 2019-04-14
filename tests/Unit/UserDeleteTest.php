e1e5c27ed71f78a03a81ab4a549c81fb159fdae7
Chandu <sa939@njit.edu>
2019-04-14 19:10:40 -0400

    7.  Create a unit test to delete a user in the database.

--- tests/Unit/UserDeleteTest.php
+++ tests/Unit/UserDeleteTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
 
 
-class UserInsertTest extends TestCase
+class UserDeleteTest extends TestCase
 {
     /**
      * A basic unit test example.
@@ -16,14 +16,11 @@ class UserInsertTest extends TestCase
      * @return void
      */
 
-
     /** @test */
-    public function userinserttest()
+    public function userdeletetest()
     {
-        $user = new User();
-        $user->name = "Chandu2";
-        $user->email = "testemai123l61231231";
-        $user->password = "3123123";
-        $this->assertTrue($user->save());
+        $user = User::find(2);
+        $this->assertTrue($user->delete());
+
     }
 }
